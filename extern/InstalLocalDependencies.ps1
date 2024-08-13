10e23bfb387b8e3ad1568029514a01b357c517bb
Fabrício Veloso <fabraccho@gmail.com>
2024-08-13 14:23:17 -0300

    Extern folder added. Localdependencies powershel script added

---
+++ extern/InstalLocalDependencies.ps1
@@ -0,0 +1,35 @@
+# Verificando se o Node.js esta instalado
+$nodePath = (Get-Command node -ErrorAction SilentlyContinue).Path
+if (-not $nodePath) {
+    Write-Host "Node.js nao esta instalado. Instalando Node.js..."
+    # Instalando Node.js via winget
+    winget install -e --id OpenJS.NodeJS
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Falha ao instalar Node.js. Saindo..."
+        exit 1
+    }
+    
+}
+else {
+    Write-Host "Node.js ja esta instalado."
+}
+
+# Verificando se o Puppeteer esta instalado globalmente
+$puppeteerInstalled = npm list -g --depth=0 | Select-String -Pattern "puppeteer"
+if (-not $puppeteerInstalled) {
+    Write-Host "Puppeteer nao esta instalado globalmente. Instalando Puppeteer..."
+    npm install -g puppeteer
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Falha ao instalar Puppeteer. Saindo..."
+        exit 1
+    }
+}
+else {
+    Write-Host "Puppeteer ja esta instalado globalmente."
+}
+
+Write-Host "Todas as instalacoes foram concluidas com sucesso."
+
+# Adiciona uma pausa para que a janela do PowerShell aguarde a entrada do usuario antes de fechar
+Write-Host "Pressione Enter para fechar a janela..."
+Read-Host
